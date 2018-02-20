791751e057cf255ecc8773d6f6709204865e3a71
Kate <bobrokat@gmail.com>
2018-02-21 00:45:40 +0300

    task 2 added

--- src/main/java/ArticleParser.java
+++ src/main/java/ArticleParser.java
@@ -23,7 +23,7 @@ public class ArticleParser {
 
         String site = "http://www.mathnet.ru";
         String url = site +
-                "/php/archive.phtml?jrnid=ivm&wshow=issue&year=2013&volume=&volume_alt=&issue=12&issue_alt=&option_lang=rus";
+                "/php/archive.phtml?jrnid=ivm&wshow=issue&year=2015&volume=&volume_alt=&issue=6&issue_alt=&option_lang=rus";
         String GET_ALL_LINKS = "//td[@width='90%']/a[@class='SLink']";
 
         WebClient webClient = new WebClient();
@@ -35,13 +35,13 @@ public class ArticleParser {
 
         try {
 
-                DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-                DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+            DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
 
-                // root element
-                Document doc = docBuilder.newDocument();
-                Element rootElement = doc.createElement("article_list");
-                doc.appendChild(rootElement);
+            // root element
+            Document doc = docBuilder.newDocument();
+            Element rootElement = doc.createElement("article_list");
+            doc.appendChild(rootElement);
             for (HtmlAnchor link : links) {
                 HtmlPage articletxt = webClient.getPage(site + link.getHrefAttribute());
 
@@ -50,9 +50,26 @@ public class ArticleParser {
                 rootElement.appendChild(article);
 
                 // title elements
-                Element title = doc.createElement("title");
-                title.appendChild(doc.createTextNode(articletxt.getFirstByXPath("//span[@class='red']/font/text()").toString()));
-                article.appendChild(title);
+
+                String titleStr = articletxt.getFirstByXPath("//span[@class='red']/font/text()").toString();
+                //default
+                Element title_default = doc.createElement("title");
+                title_default.setAttribute("type", "default");
+                title_default.appendChild(doc.createTextNode(titleStr));
+                article.appendChild(title_default);
+
+                //porter
+                Element title_porter = doc.createElement("title");
+                title_porter.setAttribute("type", "porter");
+                title_porter.appendChild(doc.createTextNode(getPorterSting(titleStr)));
+                article.appendChild(title_porter);
+
+                //mystem
+                Element title_mystem = doc.createElement("title");
+                title_mystem.setAttribute("type", "mystem");
+                title_mystem.appendChild(doc.createTextNode(getMyStemSting(titleStr)));
+                article.appendChild(title_mystem);
+
 
                 // url elements
                 Element href = doc.createElement("url");
@@ -60,38 +77,55 @@ public class ArticleParser {
                 article.appendChild(href);
 
                 // annotation elements
-                Element annotation = doc.createElement("annotation");
-                annotation.appendChild(doc.createTextNode(articletxt.getFirstByXPath("//table//text()" +
+                String annotationStr = articletxt.getFirstByXPath("//table//text()" +
                         "[preceding-sibling::b[contains(text(), 'Аннотация') " +
-                        "and following-sibling::b[1]]][1]").toString()));
-                article.appendChild(annotation);
+                        "and following-sibling::b[1]]][1]").toString();
 
+
+                //default
+                Element annotation_default = doc.createElement("annotation");
+                annotation_default.appendChild(doc.createTextNode(annotationStr));
+                annotation_default.setAttribute("type", "default");
+                article.appendChild(annotation_default);
+
+                //porter
+                Element annotation_porter = doc.createElement("annotation");
+                annotation_porter.appendChild(doc.createTextNode(getPorterSting(annotationStr)));
+                annotation_porter.setAttribute("type", "porter");
+                article.appendChild(annotation_porter);
+
+                //mystem
+                Element annotation_mystem = doc.createElement("annotation");
+                annotation_mystem.appendChild(doc.createTextNode(getMyStemSting(annotationStr)));
+                annotation_mystem.setAttribute("type", "mystem");
+                article.appendChild(annotation_mystem);
 
 
                 // keyword elements
                 String[] keywordstxt = articletxt.getFirstByXPath("//i[preceding-sibling::b[contains(text(), 'Ключевые')]]/text()").toString().split(",");
                 Element keywords = doc.createElement("keywords");
                 article.appendChild(keywords);
-                for (int i = 0; i < keywordstxt.length; i++) {
+                for (String keywordStr : keywordstxt) {
 
                     Element keyword = doc.createElement("keyword");
-                    keyword.appendChild(doc.createTextNode(keywordstxt[i]));
+                    keyword.appendChild(doc.createTextNode(keywordStr));
                     keywords.appendChild(keyword);
                 }
 
+
             }
 
-                // write the content into xml file
-                TransformerFactory transformerFactory = TransformerFactory.newInstance();
-                Transformer transformer = transformerFactory.newTransformer();
-                transformer.setOutputProperty(OutputKeys.METHOD, "xml");
-                transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-                transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
-                DOMSource source = new DOMSource(doc);
-                StreamResult result = new StreamResult(new File("file.xml"));
-                transformer.transform(source, result);
+            // write the content into xml file
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            transformer.setOutputProperty(OutputKeys.METHOD, "xml");
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(new File("task2.xml"));
+            transformer.transform(source, result);
 
-                System.out.println("File saved!");
+            System.out.println("File saved!");
 
         } catch (ParserConfigurationException pce) {
             pce.printStackTrace();
@@ -99,5 +133,26 @@ public class ArticleParser {
             tfe.printStackTrace();
         }
     }
+
+    public static String getPorterSting(String s) {
+        String[] s_arr = s.split(" ");
+        s = "";
+        Porter porter = new Porter();
+        for (String string : s_arr) {
+            s += " " + porter.stem(string);
+        }
+        return s;
     }
+
+    public static String getMyStemSting(String s) throws IOException {
+        s = s.replaceAll("[^А-Яа-я\\s]", "");
+        String[] s_arr = s.split(" ");
+        s = "";
+        MyStem myStem = new MyStem();
+        for (String string : s_arr) {
+            s += " " + myStem.stem(string);
+        }
+        return s;
+    }
+}
 
---
+++ src/main/java/MyStem.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+public class MyStem {
+
+    public String stem(String str) throws IOException {
+        Runtime rt = Runtime.getRuntime();
+        Process process = rt.exec("./mystem -ln");
+        String result;
+
+        try (InputStream in = process.getInputStream();
+             OutputStream out = process.getOutputStream();
+             BufferedReader reader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out, StandardCharsets.UTF_8))) {
+             writer.write(str + "\n");
+             writer.flush();
+             result = reader.readLine();
+        }
+        return result;
+    }
+
+}
